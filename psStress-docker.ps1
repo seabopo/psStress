ccafa8ff827c7e874aae895a899a1157fc418e05
Sean <sean@the-powells.org>
2023-06-08 02:59:32 -0500

    update for docker runs

---
+++ psStress-docker.ps1
@@ -0,0 +1,20 @@
+#==================================================================================================================
+#==================================================================================================================
+# psSTRESS - Docker Run File
+#==================================================================================================================
+#==================================================================================================================
+
+Clear-Host
+
+Set-Location -Path $PSScriptRoot
+
+Import-Module $(Split-Path $Script:MyInvocation.MyCommand.Path) -Force
+
+$params = @{
+    WarmUpTime     = $env:STRESS_WarmUpTime     ??= 2
+    CoolDownTime   = $env:STRESS_CoolDownTime   ??= 0
+    TestTime       = $env:STRESS_TestTime       ??= 60
+    StressInterval = $env:STRESS_StressInterval ??= 15
+    RestInterval   = $env:STRESS_RestInterval   ??= 10
+}
+Start-Stressing @params
--- psStress-tests.ps1
+++ psStress-tests.ps1
@@ -11,10 +11,11 @@ Set-Location -Path $PSScriptRoot
 Import-Module $(Split-Path $Script:MyInvocation.MyCommand.Path) -Force
 
 $Test = @{
-    All       = $true
+    All       = $false
     CPU       = $false
     Memory    = $false
     None      = $false
+    Docker    = $true
 }
 
 if ( $Test.All )
@@ -36,3 +37,33 @@ if ( $Test.None )
 {
     Start-Stressing -tt 30 -wt 0 -ct 0 -si 2 -ri 2 -NoCPU -NoMemory
 }
+
+if ( $Test.Docker )
+{
+    $env:STRESS_WarmUpTime     = 1
+    $env:STRESS_CoolDownTime   = 0
+    $env:STRESS_TestTime       = 10
+    $env:STRESS_StressInterval = 2
+    $env:STRESS_RestInterval   = 1
+
+    Remove-Item -Path Env:\STRESS_* -Verbose
+
+    $params = @{
+        WarmUpTime     = $env:STRESS_WarmUpTime     ??= 2
+        CoolDownTime   = $env:STRESS_CoolDownTime   ??= 0
+        TestTime       = $env:STRESS_TestTime       ??= 60
+        StressInterval = $env:STRESS_StressInterval ??= 15
+        RestInterval   = $env:STRESS_RestInterval   ??= 10
+    }
+    Start-Stressing @params
+
+    # $params = @{
+    #     WarmUpTime     = $( if ([string]::IsNullOrEmpty($env:STRESS_WarmUpTime))     {  2 } else {$env:STRESS_WarmUpTime} )
+    #     CoolDownTime   = $( if ([string]::IsNullOrEmpty($env:STRESS_CoolDownTime))   {  0 } else {$env:STRESS_CoolDownTime} )
+    #     TestTime       = $( if ([string]::IsNullOrEmpty($env:STRESS_TestTime))       { 60 } else {$env:STRESS_TestTime} )
+    #     StressInterval = $( if ([string]::IsNullOrEmpty($env:STRESS_StressInterval)) { 15 } else {$env:STRESS_StressInterval} )
+    #     RestInterval   = $( if ([string]::IsNullOrEmpty($env:STRESS_RestInterval))   { 10 } else {$env:STRESS_RestInterval} )
+    # }
+    # Start-Stressing @params -NoCPU:$env:STRESS_NoCPU -NoMemory:$env:STRESS_NoMemory
+}
+
